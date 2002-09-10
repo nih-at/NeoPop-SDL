3241b173fe6d670929f3bb1b87f0d6a83c7702fd
Thomas Klausner <tk@giga.or.at>
2002-09-10 00:24:59 +0000

    Return init success or failure; indent.

--- System_SDL/system_graphics.c
+++ System_SDL/system_graphics.c
@@ -6,7 +6,7 @@ SDL_Surface *disp;
 /* SDL Surface containing the data returned from neopop Core */
 SDL_Surface *corescr;
  
-void
+BOOL
 system_graphics_init(void)
 {
     SDL_Surface *scr;
@@ -18,11 +18,13 @@ system_graphics_init(void)
     if ((disp=SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 16,
 			       SDL_HWSURFACE|SDL_DOUBLEBUF)) == NULL) {
 	fprintf(stderr, "cannot create main window: %s\n", SDL_GetError());
+	return FALSE;
     }
 
     /* set window caption */
     SDL_WM_SetCaption(PROGRAM_NAME, NULL);
 
+#if 0
     /* make red screen */
     pixel = SDL_MapRGB(disp->format, 0xff, 0, 0);
     SDL_LockSurface(disp);
@@ -31,40 +33,40 @@ system_graphics_init(void)
     SDL_Flip(disp);
 
     sleep(4);
+#endif
 
-    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
     rm = 0x0f00;
     gm = 0x00f0;
     bm = 0x000f;
-    #else
+#else
     rm = 0x00f0;
     gm = 0x0f00;
     bm = 0xf000;
-    #endif
+#endif
     corescr = SDL_CreateRGBSurface(SDL_SWSURFACE, SCREEN_WIDTH, SCREEN_HEIGHT,
-			        16, rm, gm, bm, 0);
+				   16, rm, gm, bm, 0);
     if(corescr == NULL) {
         fprintf(stderr, "CreateRGBSurface failed: %s", SDL_GetError());
-        exit(1);
+	return FALSE;
     }
     free(corescr->pixels);
     corescr->pixels = cfb;
 
-#if 0
     pixel = SDL_MapRGB(corescr->format, 0, 0xff, 0);
     SDL_FillRect(corescr, NULL, pixel);
-#endif
 
     SDL_BlitSurface(corescr, NULL, disp, NULL);
     SDL_Flip(disp);
 
-    return;
+    return TRUE;
 }
 
 void
 system_graphics_update(void)
 {
 
+    /* update screen */
     SDL_BlitSurface(corescr, NULL, disp, NULL);
     SDL_Flip(disp);
 }

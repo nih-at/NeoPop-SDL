89d08b538869f720f5ddfbe406f819866bfc8145
Thomas Klausner <tk@giga.or.at>
2002-10-25 11:48:38 +0000

    Add some more function prototypes.

--- System_SDL/neopop-SDL.h
+++ System_SDL/neopop-SDL.h
@@ -1,5 +1,17 @@
 #include "neopop.h"
 
+BOOL system_graphics_init(void);
+void system_graphics_update(void);
+
 void system_input_update(void);
+
+void system_sound_callback(void *, Uint8 *, int);
+void system_sound_chipreset(void);
+BOOL system_sound_init(void);
+void system_sound_silence(void);
+
+void system_rom_changed(void);
+BOOL system_rom_load(char *);
+void system_rom_unload(void);
 
 extern int do_exit;

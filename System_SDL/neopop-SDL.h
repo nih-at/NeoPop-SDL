51d9008e3724f64dc300ec88c2040b3bdedb799c
Thomas Klausner <tk@giga.or.at>
2002-10-31 14:22:55 +0000

    Add function definitions.

--- System_SDL/neopop-SDL.h
+++ System_SDL/neopop-SDL.h
@@ -8,7 +8,9 @@ void system_input_update(void);
 void system_sound_callback(void *, Uint8 *, int);
 void system_sound_chipreset(void);
 BOOL system_sound_init(void);
+void system_sound_shutdown(void);
 void system_sound_silence(void);
+void system_sound_update(void);
 
 void system_rom_changed(void);
 BOOL system_rom_load(char *);

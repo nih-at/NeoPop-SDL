af1ff1cf7b8c262b496469352e8cf1e7671c9ab4
Thomas Klausner <tk@giga.or.at>
2002-12-02 14:25:23 +0000

    Add RCS Id. Use common read/write backend for all readers/writers; remove hardwired directory path for flash files.

--- System_SDL/system_io.c
+++ System_SDL/system_io.c
@@ -1,13 +1,13 @@
+/* $NiH$ */
+
+#include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
 
 #include "neopop.h"
 
-/* XXX */
-char *FlashDirectory="/home/wiz/.neopop";
-
-BOOL
-system_io_rom_read(char *filename, _u8 *buffer, _u32 len)
+static BOOL
+read_file_to_buffer(char *filename, _u8 *buffer, _u32 len)
 {
     FILE *fp;
     _u32 got;
@@ -15,96 +15,138 @@ system_io_rom_read(char *filename, _u8 *buffer, _u32 len)
     if ((fp=fopen(filename, "rb")) == NULL)
 	return FALSE;
 
-    if ((got=fread(buffer, 1, len, fp)) < len) {
-#if 0
-	fprintf(stderr, "read error (%d of %d read): %s\n", got,
-		len, strerror(errno));
-#endif
-	fclose(fp);
-	return FALSE;
-    }
+    while ((got=fread(buffer, 1, len, fp)) < len) {
+	if (feof(fp)) {
+	    fclose(fp);
+	    return FALSE;
+	}
+	if (ferror(fp) && errno != EINTR) {
+	    fclose(fp);
+	    return FALSE;
+	}
 
+	len -= got;
+	buffer += got;
+    }
+	    
     if (fclose(fp) != 0)
 	return FALSE;
 
     return TRUE;
 }
 
-/* copied from Win32 version */
+static BOOL
+write_file_from_buffer(char *filename, _u8 *buffer, _u32 len)
+{
+    FILE *fp;
+    _u32 written;
+
+    if ((fp=fopen(filename, "rb")) == NULL)
+	return FALSE;
+
+    while ((written=fwrite(buffer, 1, len, fp)) < len) {
+	if (feof(fp)) {
+	    fclose(fp);
+	    return FALSE;
+	}
+	if (ferror(fp) && errno != EINTR) {
+	    fclose(fp);
+	    return FALSE;
+	}
+
+	len -= written;
+	buffer += written;
+    }
+	    
+    if (fclose(fp) != 0)
+	return FALSE;
+
+    return TRUE;
+}
+
+
+static BOOL
+validate_dir(char *path)
+{
+    struct stat sb;
+
+    if (stat(path, &sb) == -1 && errno == ENOENT) {
+    	if (mkdir(path, 0777) == -1) {
+	    fprintf(stderr, "Config directory `%s' does not exist and "
+		    "creation failed: %s", path, strerror(errno));
+	    return FALSE;
+	}
+	if (stat(path, &sb) == -1) {
+	    fprintf(stderr, "Config directory `%s' creation failed: %s",
+		    path, strerror(errno));
+	    return FALSE;
+	}
+    }
+
+    if ((sb.st_mode & S_IFDIR) != S_IFDIR) {
+	fprintf(stderr, "Config directory `%s' is not a directory", path);
+	return FALSE;
+    }
+
+    return TRUE;
+}
+
 BOOL
-system_io_flash_read(_u8* buffer, _u32 bufferLength)
+system_io_rom_read(char *filename, _u8 *buffer, _u32 len)
+{
+    return read_file_to_buffer(filename, buffer, len);
+}
+
+BOOL
+system_io_flash_read(_u8* buffer, _u32 len)
 {
     char path[256];
-    FILE* file;
+    char *homedir;
+
+    if ((homedir=getenv("HOME")) == NULL) {
+	fprintf(stderr, "can't find home directory");
+	return FALSE;
+    }
 
     /* Build a path to the flash file */
-    snprintf(path, sizeof(path), "%s/%s.ngf", FlashDirectory,
+    snprintf(path, sizeof(path), "%s/.neopop/%s.ngf", homedir,
 	     rom.filename);
 
-    file = fopen(path, "rb");
-
-    if (file) {
-	fread(buffer, bufferLength, sizeof(_u8), file);
-	fclose(file);
-	return TRUE;
-    }
-
-    return FALSE;
+    return read_file_to_buffer(path, buffer, len);
 }
 
 BOOL
-system_io_flash_write(_u8* buffer, _u32 bufferLength)
+system_io_flash_write(_u8* buffer, _u32 len)
 {
     char path[256];
-    FILE* file;
+    char *homedir;
+    char flash_dir[256];
 
-    /* Build a path for the flash file */
-    snprintf(path, sizeof(path), "%s/%s.ngf", FlashDirectory,
-	     rom.filename);
-	
-    file = fopen(path, "wb");
-
-    if (file) {
-	fwrite(buffer, bufferLength, sizeof(_u8), file);
-	fclose(file);
-	return TRUE;
+    if ((homedir=getenv("HOME")) == NULL) {
+	fprintf(stderr, "can't find home directory");
+	return FALSE;
     }
 
-    return FALSE;
+    /* Make sure flash dir exists */
+    snprintf(flash_dir, sizeof(flash_dir), "%s/.neopop", homedir);
+    if (validate_dir(flash_dir) < 0)
+	return FALSE;
+
+    /* Build a path for the flash file */
+    snprintf(path, sizeof(path), "%s/%s.ngf", flash_dir,
+	     rom.filename);
+
+    return write_file_from_buffer(path, buffer, len);
 }
 
-//-----------------------------------------------------------------------------
-// system_io_state_read()
-//-----------------------------------------------------------------------------
-BOOL system_io_state_read(char* filename, _u8* buffer, _u32 bufferLength)
+BOOL
+system_io_state_read(char *filename, _u8 *buffer, _u32 len)
 {
-	FILE* file;
-	file = fopen(filename, "rb");
-
-	if (file)
-	{
-		fread(buffer, bufferLength, sizeof(_u8), file);
-		fclose(file);
-		return TRUE;
-	}
-
-	return FALSE;
+    return read_file_to_buffer(filename, buffer, len);
 }
 
-//-----------------------------------------------------------------------------
-// system_io_state_write()
-//-----------------------------------------------------------------------------
-BOOL system_io_state_write(char* filename, _u8* buffer, _u32 bufferLength)
+BOOL
+system_io_state_write(char *filename, _u8 *buffer, _u32 len)
 {
-	FILE* file;
-	file = fopen(filename, "wb");
-
-	if (file)
-	{
-		fwrite(buffer, bufferLength, sizeof(_u8), file);
-		fclose(file);
-		return TRUE;
-	}
-
-	return FALSE;
+    return write_file_from_buffer(filename, buffer, len);
 }

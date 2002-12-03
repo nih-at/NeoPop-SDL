d3c231ef98546a2e1b0bc44ba09f51c2fae5df7e
Thomas Klausner <tk@giga.or.at>
2002-12-03 13:08:20 +0000

    Sort and add two prototypes for state saving/loading.

--- System_SDL/neopop-SDL.h
+++ System_SDL/neopop-SDL.h
@@ -1,13 +1,19 @@
-/* $NiH$ */
+/* $NiH: neopop-SDL.h,v 1.8 2002/12/02 14:29:18 wiz Exp $ */
 
 #include "neopop.h"
 #include <SDL.h>
 
-BOOL system_graphics_init(void);
 void system_graphics_fullscreen_toggle(void);
+BOOL system_graphics_init(void);
 void system_graphics_update(void);
 
 void system_input_update(void);
+
+void system_rom_changed(void);
+BOOL system_rom_load(char *);
+void system_rom_unload(void);
+
+int system_screenshot(void);
 
 void system_sound_callback(void *, Uint8 *, int);
 void system_sound_chipreset(void);
@@ -16,11 +22,8 @@ void system_sound_shutdown(void);
 void system_sound_silence(void);
 void system_sound_update(void);
 
-void system_rom_changed(void);
-BOOL system_rom_load(char *);
-void system_rom_unload(void);
-
-int system_screenshot(void);
+void system_state_load(void);
+void system_state_save(void);
 
 extern int do_exit;
 extern int graphics_mag_req;

3de187abc194cc8c37ebf16b41abbd5895ac34cf
Thomas Klausner <tk@giga.or.at>
2003-10-15 12:30:03 +0000

    Add license template.

--- System_SDL/NeoPop-SDL.h
+++ System_SDL/NeoPop-SDL.h
@@ -1,4 +1,25 @@
-/* $NiH: neopop-SDL.h,v 1.9 2002/12/03 13:08:20 wiz Exp $ */
+/* $NiH: NeoPop-SDL.h,v 1.1 2003/10/15 12:16:38 wiz Exp $ */
+/*
+  NeoPop-SDL.h -- common header file
+  Copyright (C) 2002-2003 Thomas Klausner
+
+  This file is part of NeoPop-SDL, a NeoGeo Pocket emulator
+  The author can be contacted at <tk@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #include "neopop.h"
 #include <SDL.h>
--- System_SDL/system_rom.c
+++ System_SDL/system_rom.c
@@ -1,4 +1,25 @@
-/* $NiH: system_rom.c,v 1.5 2002/12/03 13:10:03 wiz Exp $ */
+/* $NiH: system_rom.c,v 1.6 2003/10/15 12:16:15 wiz Exp $ */
+/*
+  system_rom.c -- ROM loading support
+  Copyright (C) 2002-2003 Thomas Klausner
+
+  This file is part of NeoPop-SDL, a NeoGeo Pocket emulator
+  The author can be contacted at <tk@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #include <sys/stat.h>
 #include <errno.h>
